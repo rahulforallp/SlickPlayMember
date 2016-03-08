fca7a64bcbadded15dd7e239c3c3cb49d9e51fd3
rahulforallp <rahulforallp@gmail.com>
2016-03-08 16:58:09 +0530

    crreated

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -1,8 +1,26 @@
 package controllers
 
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.mvc._
+import model.Member
 /**
   * Created by knoldus on 8/3/16.
   */
-class LoginController {
+class LoginController extends Controller{
+  val login:Form[Member]=Form(
+    mapping(
+      "username"-> nonEmptyText,
+      "password"-> nonEmptyText
+    )(Member.apply)(Member.unapply)
+  )
 
+  def displayLogin = Action {
+    Ok(views.html.login(login))
+  }
+
+  def processLogin=Action{
+
+    Ok
+  }
 }
