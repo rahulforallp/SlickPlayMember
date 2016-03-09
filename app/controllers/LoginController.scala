6d7fb2ac7090ae6faa34f8dcdc0f42d2ffc5c77e
rahulforallp <rahulforallp@gmail.com>
2016-03-09 14:00:39 +0530

    Login

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -1,23 +1,28 @@
 package controllers
 
+import com.google.inject.Inject
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.mvc._
-import model.Member
+import model._
 import play.api.i18n.Messages.Implicits._
 import play.api.Play.current
+import scala.concurrent.duration._
+
+import scala.concurrent.{Await, Future}
+
 /**
   * Created by knoldus on 8/3/16.
   */
-class LoginController extends Controller{
-  val login:Form[Member]=Form(
-    mapping(
+class LoginController @Inject()(service:MemberRepo) extends Controller{
+  val login=Form(
+    tuple(
       "username"-> nonEmptyText,
       "password"-> nonEmptyText
-    )(Member.apply)(Member.unapply)
+    )
   )
 
-  def displayLogin = Action { implicit request =>
+  def displayLogin = Action{ implicit request =>
     Ok(views.html.login(login))
   }
 
